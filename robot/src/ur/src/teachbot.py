a699193abe38761026ba9eb11f1ad3a83bb73591
satvu <sarahvu@mit.edu>
2019-10-24 16:18:46 -0400

    wip adding go to cartesian pose

--- robot/src/ur/src/teachbot.py
+++ robot/src/ur/src/teachbot.py
@@ -7,13 +7,14 @@ from std_msgs.msg import Bool, String, Int32, Float64, Float64MultiArray, UInt16
 from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 from sensor_msgs.msg import JointState
 from control_msgs.msg import FollowJointTrajectoryAction, FollowJointTrajectoryGoal
+import ur_kinematics as ur_kin 
 import actionlib
 import sensor_msgs
 import threading
 
 from ur.msg import *
 
-from scipy.fftpack import fft 
+from scipy.fftpack import fft
 from statistics import mode, mean
 from geometry_msgs.msg import WrenchStamped
 
@@ -25,17 +26,17 @@ class Module:
     def __init__(self):
         #Initialize node
         rospy.init_node('ur_comm_node', anonymous=True)
-        self.VERBOSE = True 
-        self.last_pos = None 
+        self.VERBOSE = True
 
-        self.command_complete_topic = rospy.Publisher('/command_complete', Empty, queue_size=1) #this is for the module/browser
+        # Subscribing Topics (get commands from browser)
+        rospy.Subscriber('/GoToJointAngles', GoToJointAngles, self.cb_GoToJointAngles)
+        rospy.Subscriber('/GoToCartesianPose', GoToCartesianPose, self.cb_GoToCartesianPose)
 
-        # Subscribing Topics
-        rospy.Subscriber('/GoToJointAngles', GoToJointAngles, self.cb_GoToJointAngles) #get info from browser
-        # rospy.Subscriber('/joint_move', joint_move, self.cb_joint_move) #admittance command from browser
-
-        # Action Clients
+        # Action Clients - Publish to robot
         self.joint_traj_client = actionlib.SimpleActionClient('/scaled_pos_traj_controller/follow_joint_trajectory', FollowJointTrajectoryAction)
+
+        # Publish to browser so you know something is complete
+        self.command_complete_topic = rospy.Publisher('/command_complete', Empty, queue_size=1) #this is for the module/browser
 
         # Global Vars
         self.audio_duration = 0
@@ -44,41 +45,90 @@ class Module:
         self.devMode = False
         self.seqArr = []
 
+    '''
+    When the subscriber to "GoToJointAngles" receives the name of a constant from the browser,
+    it is evaluated in this function and parsed into a "FollowTrajectoryGoal" object and sent to the
+    action client provided by Universal Robots driver. Upon completion, it publishes to "/command-complete"
+    to let the browser know that it has finished its task.
+    '''
     def cb_GoToJointAngles(self, req):
             self.joint_traj_client.wait_for_server()
 
+            # Initialize goal
             followJoint_msg = FollowJointTrajectoryGoal()
 
+            # Initialize JointTrajectory
             traj_msg = JointTrajectory()
             traj_msg.joint_names = JOINT_NAMES
 
-            target = eval(req.name)
+            # Match the name of constant to the value defined in this file
+            # Check if you are given a name to evaluate or if you're given an array of points
+            if req.name != '': 
+                target = eval(req.name)
+            else: 
+                target = [req.j0pos, req.j1pos, req.j2pos, req.j3pos, req.j4pos, req.j5pos, req.j6pos]
 
+            # Create JointTrajectoryPoint (points that respective joint_names that were set few lines above should go to)
             jointPositions_msg = JointTrajectoryPoint()
             jointPositions_msg.positions = target
-            jointPositions_msg.time_from_start = rospy.Duration(3)
-
-            # Attempt at slowing down the robot when moving, but it did not work. 
-            # Make velocity of positions that have not changed 0 otherwise that joint will also move 
-
-            # if self.last_pos != None:
-            #     velocity_array = []
-            #     for i in range(len(JOINT_NAMES)):
-            #         if self.last_pos[i] - target[i] == 0:
-            #             velocity_array.append(0)
-            #         else:
-            #             velocity_array.append(0.001)
+            jointPositions_msg.time_from_start = rospy.Duration(3) #use this to stall so that the first position does not get skipped
 
             traj_msg.points = [jointPositions_msg,]
             followJoint_msg.trajectory = traj_msg
 
+            # Send goal to action client and wait for completion of task
             self.joint_traj_client.send_goal(followJoint_msg)
             self.joint_traj_client.wait_for_result()
 
-            self.last_pos = target
             self.command_complete_topic.publish()
-    
-## DEFINE IMPORTANT CONSTANTS ##
+
+    '''
+    Receives "GoToCartesianPose" message and based on which paramters are given, perform kinematic different
+    operations.
+    '''
+    def cb_GoToCartesianPose(self, req):
+        # Evaluate all of the paramters
+        joint_angles = eval(req.joint_angles)
+        relative_pose = eval(req.relative_pose)
+        endpoint_pose = eval(req.endpoint_pose)
+        position = eval(req.position)
+        orientation = eval(req.orientation)
+
+        if joint_angles and len(joint_angles) != len(JOINT_NAMES):
+            rospy.logerr('len(joint_angles) does not match len(JOINT_NAMES)!')
+            return None
+
+        if (position is None and orientation is None and relative_pose is None and endpoint_pose is None):
+            if joint_angles:
+                # Forward Kinematics <-- comment from the Sawyer package, but seems this is just a goToJointAngles
+                joint_input = GoToJointAngles()
+                joint_input.name = req.joint_angles
+                self.cb_GoToJointAngles(joint_input)
+
+                # call to GoToJointAngles already sends a command_complete so we can return None to escape this function 
+                return None 
+
+            else:
+                rospy.loginfo("No Cartesian pose or joint angles given.")
+                return None
+        else:
+            # Inverse Kinematics 
+            converted_joint_angles = ur_kin.inverse(endpoint_pose)
+            joint_input = GoToJointAngles()
+
+            # Set the new joint angle to go to
+            joint_input.j0pos = converted_joint_angles[0]
+            joint_input.j1pos = converted_joint_angles[1]
+            joint_input.j2pos = converted_joint_angles[2]
+            joint_input.j3pos = converted_joint_angles[3]
+            joint_input.j4pos = converted_joint_angles[4]
+            joint_input.j5pos = converted_joint_angles[5]
+
+            self.cb_GoToJointAngles(joint_input)
+
+            return None 
+
+## DEFINE IMPORTANT CONSTANTS --- MAKE SURE THEY MATCH WITH MODULE 1 OR 2 CONSTANTS ##
 if __name__ == '__main__':
     # Position CONSTANTS
     SCARA = [0, -3.14, 0, -3.14, -1.57, 0]
@@ -92,7 +142,7 @@ if __name__ == '__main__':
 
     m = Module()
 
-    try:              
+    try:
         rospy.spin()
 
     except rospy.ROSInterruptException:
