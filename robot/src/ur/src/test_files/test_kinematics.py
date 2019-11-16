cc3e2151d1b32a565a6538ee9c2ce069e3dfb54a
sarah <sarahvu@mit.edu>
2019-11-16 16:25:07 -0500

    wip test

--- robot/src/ur/src/test_files/test_kinematics.py
+++ robot/src/ur/src/test_files/test_kinematics.py
@@ -3,20 +3,36 @@ import kinematics
 
 class TestKinematicMethods(unittest.TestCase):
 
-    def test_forward_analytical(self):
-        kin_solver = Kinematics([0.1625, -0.425, -0.3992, 0.1333, 0.0997, 0.0996])
+    def test_forward_analytical_0(self):
+        ur5e_params = [0.1625, -0.425, -0.3992, 0.1333, 0.0997, 0.0996]
+        kin_solver = Kinematics(ur5e_params)
         forward_result = kin_solver.forward([])
+        self.assertTrue([], forward_result)
 
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
+    def test_forward_analytical_1(self):
+        ur5e_params = [0.1625, -0.425, -0.3992, 0.1333, 0.0997, 0.0996]
+        kin_solver = Kinematics(ur5e_params)
+        forward_result = kin_solver.forward([])
+        self.assertTrue([], forward_result)
+    
+    def test_forward_analytical_2(self):
+        ur5e_params = [0.1625, -0.425, -0.3992, 0.1333, 0.0997, 0.0996]
+        kin_solver = Kinematics(ur5e_params)
+        forward_result = kin_solver.forward([])
+        self.assertTrue([], forward_result)
+    
+    def test_inverse_analytical_0(self):
+        return 
+    
+    def test_inverse_analytical_1(self):
+        return 
+    
+    def test_inverse_analytics_outside_bounds(self):
+        return 
 
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
+    def test_inverse_analytical_singular(self):
+        return 
+
 
 if __name__ == '__main__':
     unittest.main()
