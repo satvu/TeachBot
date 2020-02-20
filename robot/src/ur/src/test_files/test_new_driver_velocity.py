3cc3effe9590140a45e4429f3f7391d3de4617f1
sarah <sarahvu@mit.edu>
2020-02-20 17:55:57 -0500

    wip with velocity

--- robot/src/ur/src/test_files/test_new_driver_velocity.py
+++ robot/src/ur/src/test_files/test_new_driver_velocity.py
@@ -10,38 +10,21 @@ from control_msgs.msg import FollowJointTrajectoryAction, FollowJointTrajectoryG
 import actionlib
 
 SCARA = [0, -3.14, 0, -3.14, -1.57, 0]
-ROTATE_WRIST = [0, -3.14, 0, -3.14, -1.57, -1]
-ROTATE_WRIST_BACK = [0, -3.14, 0, -3.14, -1.57, 0]
+ROTATE = [0, -3.14, 1.0, -3.14, -1.57, 0]
 ZERO = [0, -1.57, 0, -1.57, 0, 0]
+VEL_0 = [1.652628802228719e-10, 4.42951238155365e-10,2.825678568333387e-10, 4.566105365753174e-10, 4.458395600318909e-10, .29]
+VEL_ROTATE_1 = [0, 0, 2e-9, 0, 0, 0]
+VEL_ROTATE_2 = [0, 0, 1, 0, 0, 0]
 JOINT_NAMES = ['shoulder_pan_joint', 'shoulder_lift_joint', 'elbow_joint',
                'wrist_1_joint', 'wrist_2_joint', 'wrist_3_joint']
 
 class VelocityClient:
 
     def __init__(self):
-        self.initialized = False 
-
-        rospy.Subscriber('/ur_hardware_interface/robot_program_running', Bool, self.cb_robot_ready)
         rospy.Subscriber('/joint_states', JointState, self.send_command)
 
-        self.client = actionlib.SimpleActionClient('/scaled_pos_traj_controller/follow_joint_trajectory', FollowJointTrajectoryAction)
-        
-    def cb_robot_ready(self, res):
-        if not self.initialized:
-            self.initialized = res.data
-            if self.initialized:
-                print "inside ready and initialized"
-                rospy.wait_for_service("set_speed_slider")
-                print "connected to service"
-                try:
-                    send_speed = rospy.ServiceProxy("set_speed_slider", Float64)
-                    send_speed(0.5)
-                    print send_speed.success
+        self.client = actionlib.SimpleActionClient('/scaled_vel_traj_controller/follow_joint_trajectory', FollowJointTrajectoryAction)
                 
-                except:
-                    print "could not connect to service"
-                
-
     def send_command(self, joints):
         print 'press enter'
         pause = raw_input()
@@ -54,7 +37,14 @@ class VelocityClient:
         traj_msg.joint_names = JOINT_NAMES
 
         jointPositions_msg = JointTrajectoryPoint()
-        jointPositions_msg.positions = ZERO
+
+        # jointPositions_msg.positions = ROTATE
+        # jointPositions_msg.velocities = VEL_ROTATE_1
+
+        # jointPositions_msg.positions = SCARA
+        jointPositions_msg.velocities = VEL_ROTATE_2
+
+        jointPositions_msg.accelerations = [0, 0, 0, 0, 0, 0]
         jointPositions_msg.time_from_start = rospy.Duration(3)
 
         traj_msg.points = [jointPositions_msg,]
